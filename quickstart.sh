96c235569ab01fea5f10125d3c37743a4d7b1e7c
sahithya19 <sahithyav@biarca.com>
2019-01-31 16:35:05 +0530

    Update quickstart.sh

--- quickstart.sh
+++ quickstart.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
-echo "Hello, world! ************."
-touch $TEST_ENV.txt
+echo "Hello, world! ************ ."
+echo " $TEST_ENV*********"
+touch /tmp/$TEST_ENV.txt
+touch /root/$TEST_ENV.txt
 sleep 10000
